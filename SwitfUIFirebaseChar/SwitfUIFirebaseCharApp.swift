971d9586f26a97379312ad263b6a1468082fd089
json-kim <flutter_dart@kakao.com>
2022-05-09 22:13:31 +0900

    Initial Commit

---
+++ SwitfUIFirebaseChar/SwitfUIFirebaseCharApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitfUIFirebaseCharApp.swift
+//  SwitfUIFirebaseChar
+//
+//  Created by JeaSeung Kim on 2022/05/09.
+//
+
+import SwiftUI
+
+@main
+struct SwitfUIFirebaseCharApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
